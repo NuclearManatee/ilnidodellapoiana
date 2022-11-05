f399fcacb0b6eb79cd7749b394963b2f7e54fd54
NuclearManatee <edoardo.cremaschi@gmail.com>
2022-11-05 23:01:23 +0100

    React Leaflet Map Enabled.

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -1,4 +1,6 @@
 import type { Geo } from "../types"
+import { Marker, MapContainer, TileLayer } from "react-leaflet"; // eslint-disable-line import/no-webpack-loader-syntax
+
 
 interface Props {
     coordinates : Geo
@@ -6,14 +8,15 @@ interface Props {
 
 export default function Location( {coordinates} : Props ){
 
+    const geos : any = [coordinates.lat, coordinates.lon];
 
     return(<>
-    
-        <div>
-            <span>
-                location.
-            </span>
-        </div>
-
+        <MapContainer center={geos} zoom={12} scrollWheelZoom={false} className="map">
+        <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+            <Marker position={geos} />
+        </MapContainer>
     </>)
 }
